92d94f1c09d1fc8d84b6e04e9a9ac733eee0670c
Abdullayev-Sarvar <164860430+Abdullayev-Sarvar@users.noreply.github.com>
2024-10-07 00:10:27 +0500

    Update quotes.ts

--- src/redux/slice/quotes.ts
+++ src/redux/slice/quotes.ts
@@ -1,18 +1,22 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
-    getquo: [] || null
+interface QuotesState {
+    getquo: any[]
+}
+
+const initialState: QuotesState = {
+    getquo: []
 }
 
 const quotesSlice = createSlice({
     name: "quotes",
     initialState,
     reducers: {
-        setQuotes: (state, action: PayloadAction<any>) => {
-            state.getquo = action.payload
+        setQuotes: (state, action: PayloadAction<any[]>) => {
+            state.getquo = action.payload;
         }
     }
 })
 
-export const {setQuotes} = quotesSlice.actions;
+export const { setQuotes } = quotesSlice.actions;
 export default quotesSlice.reducer;
